fa5900ceb0e5c60d90d297ff8eceb9b00d0cb955
Diwakar Singh <diwakar@pluto.health>
2025-04-08 08:01:13 +0530

    other solution added

--- rotate-by-n.js
+++ rotate-by-n.js
@@ -13,3 +13,13 @@ var rotate = function(nums, k) {
     reverse(0, k - 1);                 
     reverse(k, nums.length - 1);      
 };
+
+// takes more time but works
+
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    let data = nums.splice(0, nums.length - k)
+    let data2 = nums.splice(-k)
+    for(let x of data2) nums.push(x)
+    for(let x of data) nums.push(x);
+};
