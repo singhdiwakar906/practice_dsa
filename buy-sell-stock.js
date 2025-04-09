616889ba86fb8d7e44e45475e50c649899424c34
Diwakar Singh <diwakar@pluto.health>
2025-04-09 07:31:26 +0530

    best time to buy and sell stock solved

---
+++ buy-sell-stock.js
@@ -0,0 +1,14 @@
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+    }
+
+    return maxProfit;
+};
