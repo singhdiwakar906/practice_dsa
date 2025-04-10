b025215eb0094b74834f34a30885b5c6df4a01bd
Diwakar Singh <diwakar@pluto.health>
2025-04-10 09:06:52 +0530

    jump game code added

---
+++ jump-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+    let farthest = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > farthest) return false;
+        farthest = Math.max(farthest, i + nums[i]);
+    }
+
+    return true;
+
+};
